e606b68aa2ad2758a27e6baa538e73bcc5bfee2b
JesCat16 <jmdjesus83@gmail.com>
2025-05-06 13:30:06 -0300

    Atualização das portas
    Achei um jeito de ter um script fixo para todas as portas. E um jeito de manter o inventario e caixas de falas, mas por enquanto só é as portas que foram alteradas

---
+++ O Segredo da Fazenda Abandonada/Assets/Script/Portas.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portas : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Load the specified scene
+            //SceneManager.LoadScene(sceneToLoad);
+            Debug.Log("Bateu na porta");
+        }
+    }
+}
